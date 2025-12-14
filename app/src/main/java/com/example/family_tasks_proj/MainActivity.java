e0ff7c2351fa7a7f8b7d666fa6eef2c74f5c3994
Roeip2000 <roeicalendar34@gmail.com>
2025-12-14 09:56:30 +0200

    Second commit 14.12.2025 10:00

--- app/src/main/java/com/example/family_tasks_proj/MainActivity.java
+++ app/src/main/java/com/example/family_tasks_proj/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.family_tasks_proj;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+     Button btnRegister, btnLogin, btnChildQR;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnRegister = findViewById(R.id.btnRegister);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnChildQR = findViewById(R.id.btnChildQR);
 
+        // מעבר להרשמה להורים
+        btnRegister.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ParentsRegisterActivity.class);
+            startActivity(intent);
+        });
+
+        // מעבר להתחברות הורים
+        btnLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ParentsLoginActivity.class);
+            startActivity(intent);
+        });
+
+        // מעבר לכניסת ילד עם QR
+        btnChildQR.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ChildQRLoginActivity.class);
+            startActivity(intent);
+        });
 
 
 
