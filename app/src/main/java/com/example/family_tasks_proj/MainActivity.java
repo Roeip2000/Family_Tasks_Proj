cc1ec39420ec386578d9c3c9ac2544eeb163e123
Roeip2000 <roeicalendar34@gmail.com>
2025-12-29 13:51:40 +0200

    Fix main screen button layout

--- app/src/main/java/com/example/family_tasks_proj/MainActivity.java
+++ app/src/main/java/com/example/family_tasks_proj/MainActivity.java
@@ -4,15 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-     Button btnRegister, btnLogin, btnChildQR;
+    private Button btnRegister;
+    private Button btnLogin;
+    private Button btnChildQR;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,9 +37,5 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, ChildQRLoginActivity.class);
             startActivity(intent);
         });
-
-
-
-
     }
 }
